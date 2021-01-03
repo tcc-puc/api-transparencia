4c33166028f36d781b9b43bde1d8971ff05bee56
Luan Fagundes <luan.fagundes@pebmed.com.br>
2021-01-03 13:32:45 -0300

    padrionizacao api

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
+const mocks = require('./mocks.json');
 const express = require("express");
 
-const app = express();
+const server = express();
 
-app.get("/", (req, res) => {
+server.use(express.json());
+
+server.get("/", (req, res) => {
   res.send("API transparencia running...");
 });
 
-app.listen(3031);
+server.get("/recursos/:action", (req, res) => {
+
+  const { action } = req.params;
+
+  if (action === "licitacoes") {
+    return res.status(200).send(mocks.licitacoes)
+
+  } else if (action === "convenios") {
+    return res.status(200).send(mocks.convenios)
+  } else {
+    return res.status(404).json({
+        "error": "search-0002",
+        "message": "Dados nāo encontrados, por favor tente novamente.",
+        "detail": "Parece que houve um erro com a sua busca",
+        "help": ""
+    })
+  }
+});
+
+server.listen(3031);
