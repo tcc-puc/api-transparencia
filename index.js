6808465868789fdec53402f2becc5972989ea4f7
Karol Harumi <karol.harumi@hotmail.com>
2021-01-17 15:19:37 -0300

    Initial config Kafkajs

--- index.js
+++ index.js
@@ -4,6 +4,44 @@ const server = express();
 
 server.use(express.json());
 
+/// Apache Kafka
+const { Kafka } = require('kafkajs')
+ 
+const kafka = new Kafka({
+  clientId: 'my-app',
+  brokers: ['kafka1:19092']
+})
+ 
+const producer = kafka.producer()
+const consumer = kafka.consumer({ groupId: 'test-group' })
+ 
+const run = async () => {
+  // Producing
+  await producer.connect()
+  await producer.send({
+    topic: 'topic1',
+    messages: [
+      { value: 'Hello KafkaJS user!' },
+    ],
+  })
+ 
+  // Consuming
+  await consumer.connect()
+  await consumer.subscribe({ topic: 'topic1', fromBeginning: true })
+ 
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        partition,
+        offset: message.offset,
+        value: message.value.toString(),
+      })
+    },
+  })
+}
+ 
+run().catch(console.error);
+
 /**
  * Redis config
  */
